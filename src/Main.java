5bbe53bbad08adddc1d85e075b05f8c9820a55d7
Vishnu <vishnunair20012112@gmail.com>
2023-06-05 12:55:23 +0530

    shifting zeros to the end

--- src/Main.java
+++ src/Main.java
@@ -9,28 +9,71 @@ public class Main {
 
     public static  void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = {1, 2, 3, 4, 5};
-        int k=3;
-        left_rotation_by_K(arr,k);
+        int arr[] = {1 ,0 ,2 ,3 ,0 ,4 ,0 ,1};
+         Move_zeros_to_the_end(arr);
+
     }
-    public static  void left_rotation_by_K(int arr[],int k){
-        System.out.println("Array before rotating: ");
-        for(int i : arr){
-            System.out.print(i+" ");
-        }
-    int temp;
-        for(int i =0;i<k;i++){
-        for(int j=0;j< arr.length-1;j++){
-            temp=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
-        }
-    }
+   public static void  move_zeros_to_the_end(int array[]){
+        System.out.print("Array before shifting: ");
+        for(int i:array){System.out.print(i+" ");}
         System.out.println();
-        System.out.print("Array after rotating "+k+" times: ");
-        for(int i:arr){
+        int var =array.length;
+        int zeros;
+        if(array[var-1]==0){
+            zeros=1;
+            int temp;
+            for(int i =var-1;i>=0;i--){
+                if(array[i]==0){
+                    for(int j=i;j<var-zeros-1;j++){
+                        temp=array[j];
+                        array[j]=array[j+1];
+                        array[j+1]=temp;
+                    }
+                }
+                zeros++;
+            }
+        }else
+        {
+            zeros=0;
+            int temp;
+            for(int i=var-1;i>=0;i--){
+                if(array[i]==0){
+                    for(int j=i;j<var-zeros-1;j++){
+                        temp=array[j];
+                        array[j]=array[j+1];
+                        array[j+1]=temp;
+                    }
+                }
+                zeros++;
+            }
+        }
+        System.out.print("Array after shifting: ");
+        for(int i : array){
             System.out.print(i+" ");
-        };
-    }
+        }
+   }
+
+   public static void Move_zeros_to_the_end(int arr[]){
+
+         System.out.print("Array before shifting: ");
+         for(int i=0;i< arr.length;i++){
+             System.out.print(arr[i]+" ");
+         };
+        int array[] = new int[arr.length];
+       int index = 0;
+        for(int i=0;i< arr.length;i++){
+            if(arr[i]!=0){
+
+             array[index]=arr[i];
+             index++;
+
+            }
+        }
+        System.out.println();
+        System.out.print("Array after shifting: ");
+        for(int i=0;i< array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+   }
 }
 
