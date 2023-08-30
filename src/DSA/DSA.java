0639fee4bee1752f384abefa53f6e28cd6137b8a
Vishnu <vishnunair20012112@gmail.com>
2023-08-30 13:02:50 +0530

    Appears Once

--- src/DSA/DSA.java
+++ src/DSA/DSA.java
@@ -1,41 +1,40 @@
 package DSA;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class DSA {
 
     public static void main(String[] args) {
-        int array[] = {1,0,1,1,1,0,1,1,1,1,0,0,0,0,1,1,1,1,1,1,0,0,1,1,1};
-        int result = maxConsicutiveOne(array);
-        System.out.print(result);
-
-
+        int[] arr = {2,2,1};
+        DSA dsa = new DSA();
+        System.out.println(dsa.frequencyChecker(arr));
     }
-    static int maxConsicutiveOne(int array[]){
-        int iterator=0;
-        int consicutivechecker=0;
+    public int  frequencyChecker(int[] arr){
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for(int i = 0;i<arr.length;i++){
 
-        for(int i=0;i<array.length;i++){
-            if(array[i]==1){
-                iterator++;
+            if(hashMap.containsKey(arr[i])){
+                hashMap.replace(arr[i],hashMap.get(arr[i])+1);
+            }else{
+                hashMap.put(arr[i], 1);
+            }
 
+        }
+        System.out.println(hashMap);
+        int AppearsOnce=0;
+
+
+        for(int i=0;i< arr.length;i++){
+            if(hashMap.get(arr[i])==1){
+                AppearsOnce = arr[i];
+
+                break;
 
             }
-            else{
-                    if(iterator>consicutivechecker) {
-                         consicutivechecker=iterator;
-                         iterator=0;
-                       }
-                }
 
-            if(i==array.length-1){
-                if(iterator>consicutivechecker){
-                    consicutivechecker=iterator;
-
-                }            }
         }
 
-
-        return consicutivechecker;
+        return AppearsOnce;
     }
 }
