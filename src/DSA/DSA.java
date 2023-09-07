6602be52944aa7019c96c06234100d276f515c00
Vishnu <vishnunair20012112@gmail.com>
2023-09-07 11:35:38 +0530

    Second Largest And Smallest

--- src/DSA/DSA.java
+++ src/DSA/DSA.java
@@ -1,40 +1,54 @@
 package DSA;
 
+import java.lang.reflect.Array;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class DSA {
 
     public static void main(String[] args) {
-        int[] arr = {2,2,1};
+        int[] arr = {1,2,4,7,7,5};
+        int i=Integer.MAX_VALUE;
+
         DSA dsa = new DSA();
-        System.out.println(dsa.frequencyChecker(arr));
-    }
-    public int  frequencyChecker(int[] arr){
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for(int i = 0;i<arr.length;i++){
+        dsa.SecondSmallestAndLargest(arr);
 
-            if(hashMap.containsKey(arr[i])){
-                hashMap.replace(arr[i],hashMap.get(arr[i])+1);
-            }else{
-                hashMap.put(arr[i], 1);
+    }
+
+   public void SecondSmallestAndLargest(int[] arr){
+        if(arr.length<4){
+            System.out.println(-1);
+        }
+        else{
+            int LARGEST=arr[0];
+            int SMALLEST=arr[0];
+
+            for(int i = 0;i<arr.length;i++){
+                if(arr[i]>LARGEST){
+                    LARGEST=arr[i];
+                }
+                else if(arr[i]<SMALLEST){
+                    SMALLEST=arr[i];
+                }
             }
+            int SECOND_LARGEST=SMALLEST;
+            int SECOND_SMALLEST=LARGEST;
+
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]<LARGEST && arr[i]>SECOND_LARGEST){
+                    SECOND_LARGEST=arr[i];
+                }
+                if(arr[i]>SMALLEST && arr[i]<SECOND_SMALLEST){
+                    SECOND_SMALLEST=arr[i];
+                }
+            }
+            HashMap<String,Integer> SecondLargestAndSmallest=new HashMap<>();
+            SecondLargestAndSmallest.put("SecondLargest : ",SECOND_LARGEST);
+            SecondLargestAndSmallest.put("SecondSmallest :",SECOND_SMALLEST);
+            System.out.println(SecondLargestAndSmallest);
 
         }
-        System.out.println(hashMap);
-        int AppearsOnce=0;
 
+   }
 
-        for(int i=0;i< arr.length;i++){
-            if(hashMap.get(arr[i])==1){
-                AppearsOnce = arr[i];
-
-                break;
-
-            }
-
-        }
-
-        return AppearsOnce;
-    }
 }
